fee15cf899d1f9c9a5b9e55cff735b4756fc0e63
Iliyan-Y <maximizebg@gmail.com>
2020-08-05 19:06:46 +0300

    Chapter 12 we are stuck

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -3,10 +3,11 @@ class DockingStation
   attr_reader :bike
 
   def release_bike
-    Bike.new
+    @bike
   end
 
   def dock(bike)
+    # fail 'Docking station full' if @bike
     @bike = bike
   end
 end
